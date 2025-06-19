4c8437eb1e71b50edd09054f245714a2a54cd423
Palikaghai <149198562+Palikaghai@users.noreply.github.com>
2025-06-19 21:04:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+from chatbot_model import get_chatbot_response
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    chatbot_response = None
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+        chatbot_response = get_chatbot_response(user_input)
+    return render_template("index.html", response=chatbot_response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
